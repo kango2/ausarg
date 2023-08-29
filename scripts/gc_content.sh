e42f00df31a63ec237e4b4526f8234d36a3dacb6
kiratalreja3 <kiratalreja@gmail.com>
2023-08-29 14:51:21 +1000

    added GC plotter

---
+++ scripts/gc_content.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -N gc_calculation
+#PBS -P xl04
+#PBS -q express
+#PBS -l walltime=00:30:00
+#PBS -l mem=32GB
+#PBS -l ncpus=48
+#PBS -l storage=gdata/xl04+gdata/if89
+#PBS -l wd
+#PBS -l jobfs=100GB
+#PBS -M kirat.alreja@anu.edu.au
+
+#TODO : Add usage and flags for public use
+
+module load parallel/20191022 biopython/1.79 kentutils/0.0
+
+inputfile="$input"
+outputdir="$output"
+
+faSplit sequence $inputfile 10000 ${PBS_JOBFS}/chunk
+
+cd ${PBS_JOBFS}
+filelist=$(ls ${PBS_JOBFS}/chunk*)
+printf "%s\n" "${filelist[@]}" | parallel -I{} --jobs ${PBS_NCPUS} python3 /g/data/xl04/ka6418/ausarg/scripts/gc_content_helper.py {} 
+
+awk 'NR==1{print; next} FNR>1' *.csv > "${outputdir}/$(basename "$inputfile")_GC.csv"
