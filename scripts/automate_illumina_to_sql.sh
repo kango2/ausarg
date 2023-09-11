4483d225db41dd7344f21ad6cfb57253bfe03995
kiratalreja3 <kiratalreja@gmail.com>
2023-09-11 15:02:06 +1000

    added upodates to illumina automation

--- scripts/automate_illumina_to_sql.sh
+++ scripts/automate_illumina_to_sql.sh
@@ -2,4 +2,4 @@
 
 
 
-for i in `sqlite3 /g/data/xl04/ka6418/ausarg/database/ausarg.db 'select file_path from Illumina_Metrics where status == "Pending"'`; do j=$(echo $i | sed 's/;/:/'); echo qsub -v filepair=$j,output=/g/data/xl04/bpadata/Bassiana_duperreyi/raw/evaluation/illumina_qc run_illumina_qc.sh; done
+for i in `sqlite3 /g/data/xl04/ka6418/ausarg/database/ausarg.db 'select file_path from Illumina_Metrics where status == "Pending"'`; do j=$(echo $i | sed 's/;/:/'); qsub -v filepair=$j,output=/g/data/xl04/bpadata/Pogona_vitticeps/raw/evaluation/illumina_qc /g/data/xl04/ka6418/ausarg/scripts/run_illumina_qc.sh; done
