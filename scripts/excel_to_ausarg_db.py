c9b4c4b7452106e1bddb2f66ff5bc1dc9ef1f561
kiratalreja3 <kiratalreja@gmail.com>
2023-09-11 13:57:48 +1000

    added illumina metrics feature to initialisation script

--- scripts/excel_to_ausarg_db.py
+++ scripts/excel_to_ausarg_db.py
@@ -1,6 +1,39 @@
 import sqlite3
 import pandas as pd
 import argparse
+import sqlite3
+
+def get_illumina_filenames_from_sra(db_path):
+    """Retrieve filenames from the SRA table."""
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT filename FROM SRA where library_source=='GENOMIC' AND platform=='ILLUMINA'")
+    filenames = cursor.fetchall()
+    conn.close()
+    
+    # Flatten the filenames list and remove None entries
+    flat_filenames = [f for sublist in filenames for f in sublist if f]
+    
+    return flat_filenames
+
+def populate_illumina_metrics(db_path, filenames):
+    """Insert filenames into the illumina_metrics table with status set to 'Pending', avoiding duplicates using INSERT OR IGNORE."""
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    
+    rows_inserted = 0  # Counter for rows inserted
+    
+    for filename in filenames:
+        cursor.execute("INSERT OR IGNORE INTO illumina_metrics (File_path, status) VALUES (?, 'Pending')", (filename,))
+        
+        # If a row was inserted, the cursor's rowcount attribute will be 1. Otherwise, it'll be -1.
+        if cursor.rowcount == 1:
+            rows_inserted += 1
+            
+    conn.commit()
+    conn.close()
+    
+    print(f"{rows_inserted} rows inserted into illumina_metrics.")
 
 def main(db_path, sra_data_path):
     # Create a new SQLite database
@@ -12,12 +45,17 @@ def main(db_path, sra_data_path):
 
     # Insert the data from the "SRA_data" sheet into the Sample table
     updated_sra_data.to_sql('SRA', conn, if_exists='replace', index=False)
+    
+    #Initialise the illumina metrics table
+    populate_illumina_metrics(db_path,get_illumina_filenames_from_sra(db_path))
 
     # Close the database connection
     conn.close()
 
     # Confirm that data insertion is complete
     print("Data insertion into the SRA table is complete.")
+
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Insert data from an Excel file into an SQLite database.")
