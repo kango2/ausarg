ce3b9602a051ff5396d75a07ed0c56f1269fd37a
kiratalreja3 <kiratalreja@gmail.com>
2023-09-14 14:31:46 +1000

    added trash to plotly

--- scripts/depth_calculater.sh
+++ scripts/depth_calculater.sh
@@ -16,28 +16,22 @@ reference="$reference_fasta"
 output="$outputname"
 technology="$tech"  # PacBio, ONT, or Illumina
 
-# Step 1: Align reads to the reference genome based on technology
 IFS=':' read -ra ADDR <<< "$rawreads"
 for ((i=0; i<${#ADDR[@]}; i++)); do
     read_file="${ADDR[$i]}"
     case $technology in
         PacBio)
-            minimap2 -t 48 -ax map-pb "$reference" "$read_file" >> "$outputname.sam"
+            minimap2 -t 48 -ax map-pb "$reference" "$read_file" >> "${output}.sam"
             ;;
         ONT)
-            minimap2 -t 48 -ax map-ont "$reference" "$read_file" >> "$outputname.sam"
+            minimap2 -t 48 -ax map-ont "$reference" "$read_file" >> "${output}.sam"
             ;;
         Illumina)
             # Assuming paired-end reads: process read_file and the next file in the ADDR array as its R2
-            if [[ $((i+1)) -lt ${#ADDR[@]} ]]; then
-                read_file2="${ADDR[$((i+1))]}"
-                minimap2 -t 48 -ax sr "$reference" "$read_file" "$read_file2" >> "$outputname.sam"
-                # Increment the counter to skip the next file since it's already processed as R2
-                ((i++))
-            else
-                echo "Mismatch in paired-end files. Exiting."
-                exit 1
-            fi
+            read_file2="${ADDR[$i+1]}"
+            minimap2 -t 48 -a "$reference" "$read_file" "$read_file2" >> "${output}.sam"
+            # Increment the counter to skip the next file since it's already processed as R2
+            i=$((i+1))
             ;;
         *)
             echo "Unknown sequencing technology. Exiting."
@@ -46,18 +40,19 @@ for ((i=0; i<${#ADDR[@]}; i++)); do
     esac
 done
 
+
 # Step 2: Convert SAM to BAM
-samtools view -bS "$outputname.sam" > "$outputname.bam"
+samtools view -bS "${output}.sam" > "${output}.bam"
 
 # Step 3: Sort and index BAM file
-samtools sort "$outputname.bam" -o "${outputname}_sorted.bam"
-samtools index "${outputname}_sorted.bam"
+samtools sort -@ 48 "${output}.bam" -o "${output}_sorted.bam"
+samtools index "${output}_sorted.bam"
 
 # Clean up intermediate files
-rm "$outputname.sam" "$outputname.bam"
+rm "${output}.sam" "${output}.bam"
 
 # Step 4: Generate binned depth over 1000 base intervals in CSV format
-samtools depth -a "${outputname}_sorted.bam" | awk 'BEGIN {OFS=","; print "Chromosome","Start","End","AverageDepth"} {
+samtools depth -a "${output}_sorted.bam" | awk 'BEGIN {OFS=","; print "Chromosome","Start","End","AverageDepth"} {
     total+=$3;
     if (NR % 1000 == 0) {
         print $1, NR-999, $2, total/1000;
@@ -68,4 +63,4 @@ END {
     if (NR % 1000 != 0) {
         print $1, NR-(NR%1000)+1, $2, total/(NR%1000);
     }
-}' > "${outputname}_sorted.bam.binned.depth.csv"
+}' > "${output}_sorted.bam.binned.depth.csv"
