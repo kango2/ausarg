5992540772bb1675db7e6ed39dd93c76f65ed40c
kiratalreja3 <kiratalreja@gmail.com>
2023-10-13 14:09:23 +1100

    fixed asmqc

--- scripts/genejigsaw/asm_qc/asm_qc.sh
+++ scripts/genejigsaw/asm_qc/asm_qc.sh
@@ -40,28 +40,24 @@ if [ -z "${fasta}" ] || [ -z "${illumina}" ] || [ -z "${pacbio}" ] || [ -z "${on
     usage
 fi
 
+ref_base=$(basename "${fasta}" .fasta)
+ref=${fasta}
 gc=/g/data/xl04/ka6418/github/ausarg/scripts/gc_content.sh 
 telomere=/g/data/xl04/ka6418/github/ausarg/scripts/find_telomeres.sh
 centromere=/g/data/xl04/ka6418/github/ausarg/scripts/centromeres.sh
 read_depth=/g/data/xl04/ka6418/github/ausarg/scripts/align_and_depth_launcher_v2.sh
 seq_table=/g/data/xl04/ka6418/github/ausarg/scripts/asm_to_sequencetable.py
 
-qsub -P ${project} -l "storage=${storage}" -N "${ref_base}_GC" -j oe -o ${logsdir} -v input=${fasta},output=${outputdir} ${gc} 
+echo qsub -P ${project} -l "storage=${storage}" -N "${ref_base}_GC" -j oe -o ${logsdir} -v input=${fasta},output=${outputdir} ${gc} 
 
-qsub -P ${project} -l "storage=${storage}" -N "${ref_base}_Tel" -j oe -o ${logsdir} -v input=${fasta},output=${outputdir},permatch=90,copies=100 ${telomere}
+echo qsub -P ${project} -l "storage=${storage}" -N "${ref_base}_Tel" -j oe -o ${logsdir} -v input=${fasta},output=${outputdir},permatch=90,copies=100 ${telomere}
 
-qsub -P ${project} -l "storage=${storage}" -N "${ref_base}_Cen" -j oe -o ${logsdir} -v inputfasta=${fasta},outputdir=${outputdir} ${centromere}
+echo qsub -P ${project} -l "storage=${storage}" -N "${ref_base}_Cen" -j oe -o ${logsdir} -v inputfasta=${fasta},outputdir=${outputdir} ${centromere}
 
 mkdir -p ${outputdir}/"${ref_base}_depth"
 
-qsub -N "${ref_base}_illumina" -o ${logsdir} -P ${project} -l "storage=${storage}" -v platform=illumina,rawreads=${illumina},reference=${ref},output=${outputdir}/"${ref_base}_depth"/${ref_base}_illumina /g/data/xl04/ka6418/github/ausarg/scripts/align_and_depth.sh
-qsub -N "${ref_base}_ont" -o ${logsdir} -P ${project} -l "storage=${storage}" -v platform=ont,rawreads=${ont},reference=${ref},output=${outputdir}/"${ref_base}_depth"/${ref_base}_ont /g/data/xl04/ka6418/github/ausarg/scripts/align_and_depth.sh
-qsub -N "${ref_base}_pacbio" -o ${logsdir} -P ${project} -l "storage=${storage}" -v platform=pacbio,rawreads=${pacbio},reference=${ref},output=${outputdir}/"${ref_base}_depth"/${ref_base}_pacbio /g/data/xl04/ka6418/github/ausarg/scripts/align_and_depth.sh
+qsub -q express -N "${ref_base}_illumina" -o ${logsdir} -P ${project} -l "storage=${storage}" -v platform=illumina,rawreads=${illumina},reference=${ref},output=${outputdir}/"${ref_base}_depth"/${ref_base}_illumina /g/data/xl04/ka6418/github/ausarg/scripts/align_and_depth.sh
+qsub -q express -N "${ref_base}_ont" -o ${logsdir} -P ${project} -l "storage=${storage}" -v platform=ont,rawreads=${ont},reference=${ref},output=${outputdir}/"${ref_base}_depth"/${ref_base}_ont /g/data/xl04/ka6418/github/ausarg/scripts/align_and_depth.sh
+qsub -q express -N "${ref_base}_pacbio" -o ${logsdir} -P ${project} -l "storage=${storage}" -v platform=pacbio,rawreads=${pacbio},reference=${ref},output=${outputdir}/"${ref_base}_depth"/${ref_base}_pacbio /g/data/xl04/ka6418/github/ausarg/scripts/align_and_depth.sh
 
-python3 ${seq_table} -fasta ${fasta} -outputdir ${outputdir}
-
-
-
-
-
-
+#python3 ${seq_table} -fasta ${fasta} -outputdir ${outputdir}
--- scripts/genejigsaw/asm_qc/test.sh
+++ scripts/genejigsaw/asm_qc/test.sh
@@ -1,11 +1,9 @@
-fasta=/g/data/xl04/ka6418/LamDe/hic_scaffolding/yahs/LamDe_HifiASM_YAHS.fasta \
-illumina="/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354040_AusARG_UNSW_HJTCJDSX3_ATCGGCGAAG-TCCAAGAATT_S1_L004_R1_001.fastq.gz;/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354040_AusARG_UNSW_HJTCJDSX3_ATCGGCGAAG-TCCAAGAATT_S1_L004_R2_001.fastq.gz:/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354041_AusARG_UNSW_HJTCJDSX3_CCGTGACCGA-CCGAACGTTG_S2_L004_R1_001.fastq.gz;/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354041_AusARG_UNSW_HJTCJDSX3_CCGTGACCGA-CCGAACGTTG_S2_L004_R2_001.fastq.gz:/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354042_AusARG_UNSW_HJTCJDSX3_TATCCGAGGC-GGCCAATAAG_S3_L004_R1_001.fastq.gz;/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354042_AusARG_UNSW_HJTCJDSX3_TATCCGAGGC-GGCCAATAAG_S3_L004_R2_001.fastq.gz" \
-pacbio=/g/data/xl04/bpadata/Lampropholis_delicata/raw/pacbio/350750_AusARG_AGRF_DA064169.ccs.fq.gz:/g/data/xl04/bpadata/Lampropholis_delicata/raw/pacbio/350750_AusARG_AGRF_DA095606.ccs.fq.gz \
-ont=/g/data/xl04/ka6418/LamDe/raw/ont/lamde_ont.fastq.gz \
-outputdir=/g/data/xl04/ka6418/genejigsaw_testing/lamde_yahs \
-logsdir=/g/data/xl04/ka6418/genejigsaw_testing/lamde_yahs/logs \
-project=xl04 \
-storage=gdata/if89+gdata/xl04 \
-./qc_and_plot.sh
-
-
+./asm_qc.sh \
+-f /g/data/xl04/ka6418/LamDe/hic_scaffolding/yahs/LamDe_HifiASM_YAHS.fasta \
+-i "/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354040_AusARG_UNSW_HJTCJDSX3_ATCGGCGAAG-TCCAAGAATT_S1_L004_R1_001.fastq.gz;/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354040_AusARG_UNSW_HJTCJDSX3_ATCGGCGAAG-TCCAAGAATT_S1_L004_R2_001.fastq.gz:/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354041_AusARG_UNSW_HJTCJDSX3_CCGTGACCGA-CCGAACGTTG_S2_L004_R1_001.fastq.gz;/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354041_AusARG_UNSW_HJTCJDSX3_CCGTGACCGA-CCGAACGTTG_S2_L004_R2_001.fastq.gz:/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354042_AusARG_UNSW_HJTCJDSX3_TATCCGAGGC-GGCCAATAAG_S3_L004_R1_001.fastq.gz;/g/data/xl04/ka6418/LamDe/raw/illumina/dnaseq/354042_AusARG_UNSW_HJTCJDSX3_TATCCGAGGC-GGCCAATAAG_S3_L004_R2_001.fastq.gz" \
+-p /g/data/xl04/bpadata/Lampropholis_delicata/raw/pacbio/350750_AusARG_AGRF_DA064169.ccs.fq.gz:/g/data/xl04/bpadata/Lampropholis_delicata/raw/pacbio/350750_AusARG_AGRF_DA095606.ccs.fq.gz \
+-o /g/data/xl04/ka6418/LamDe/raw/ont/lamde_ont.fastq.gz \
+-d /g/data/xl04/ka6418/genejigsaw_testing/lamde_yahs \
+-l /g/data/xl04/ka6418/genejigsaw_testing/lamde_yahs/logs \
+-P xl04 \
+-s gdata/if89+gdata/xl04
