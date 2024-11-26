10ca9278120f7337ffd44ddcc317095387bcea90
Hardip Patel <hardip.patel@anu.edu.au>
2024-11-26 14:18:00 +1100

    ribofinder version 0.1.0 complete. produces rDNA m orphs and bed files for rDNA regions

--- scripts/python/rdnabuilder.py
+++ scripts/python/rdnabuilder.py
@@ -33,40 +33,34 @@ def read_paf_file(file_path):
 
 def process_paf_alignments(data_df):
     
-    # Filter out short alignments
-    data_df = data_df[data_df['Alignment block length'].astype(float) / data_df['Target sequence length'].astype(float) > 0.50]
+    # Filter out short alignments, retaining alignments that cover at least 95% of the target sequence
+    data_df = data_df[data_df['Alignment block length'].astype(float) / data_df['Target sequence length'].astype(float) >= 0.95]
+    data_df = data_df[data_df['Number of residue matches'].astype(float) / data_df['Target sequence length'].astype(float) >= 0.90]
 
     # Ensure 18S is followed by 28S or 28S is followed by 18S for each query sequence name
+    # Current logic captures 28S-18S units. we need to change it to 18S-28S units.
     morphs = set()
     for query_name, group in data_df.groupby("Query sequence name"):
         group = group.sort_values(by=["Query start"]).reset_index(drop=True)
         valid_group = set()
         i = 0
         while i < len(group):
-            # start with a 28S in the plus strand
-            if '_28S' in group.at[i, 'Target sequence name'] and group.at[i, 'Relative strand'] == "+":
-                #check if the next one is a valid 18S
-                if i + 1 < len(group) and '_18S' in group.at[i + 1, 'Target sequence name'] and group.at[i, 'Relative strand'] == group.at[i + 1, 'Relative strand']:
-                    # check if the next one is a valid 28S
-                    if i + 2 < len(group) and '_28S' in group.at[i + 2, 'Target sequence name'] and group.at[i + 1, 'Relative strand'] == group.at[i + 2, 'Relative strand']:
-                        # check if 28S alignment is not on the edge of the contig and that there is 100bp available after the i+2 28S
-                        if group.at[i + 2, 'Query end'] + 100 < group.at[i + 2, 'Query sequence length']:
-                            morph = group.at[i, 'Query sequence name'], group.at[i, 'Query end'] + 100, group.at[i + 2, 'Query end'] + 100, '+'
-                            morphs.add(morph)
-                            # morph = group.at[i, 'Query sequence name'] + "\t" + str(group.at[i, 'Query end'] + 100) + "\t" + str(group.at[i + 2, 'Query end'] + 100) + "\t+"
-                            # morphs.add(morph)
-            # start with a 28S in the minus strand
-            elif '_28S' in group.at[i, 'Target sequence name'] and group.at[i, 'Relative strand'] == "-":
-                # check if 28S alignment is not on the edge of the contig and that there is 100bp available before the query start
-                if group.at[i, 'Query start'] - 100 > 0:
-                    #check if the next one is a valid 18S
-                    if i + 1 < len(group) and '_18S' in group.at[i + 1, 'Target sequence name'] and group.at[i, 'Relative strand'] == group.at[i + 1, 'Relative strand']:
-                        # check if the next one is a valid 28S
-                        if i + 2 < len(group) and '_28S' in group.at[i + 2, 'Target sequence name'] and group.at[i + 1, 'Relative strand'] == group.at[i + 2, 'Relative strand']:
-                            morph = group.at[i, 'Query sequence name'], group.at[i, 'Query start'] - 100, group.at[i + 2, 'Query start'] - 100, '-'
-                            morphs.add(morph)
-                            # morph = group.at[i, 'Query sequence name'] + "\t" + str(group.at[i, 'Query start'] - 100) + "\t" + str(group.at[i + 2, 'Query start'] - 100) + "\t-"
-                            # morphs.add(morph)
+            # start with a 18S in the plus strand, and 100bp available before this 18S on the contig
+            if '_18S' in group.at[i, 'Target sequence name'] and group.at[i, 'Relative strand'] == "+" and group.at[i, 'Query start'] - 100 > 0:
+                #check if the next one is a valid 28S
+                if i + 1 < len(group) and '_28S' in group.at[i + 1, 'Target sequence name'] and group.at[i + 1, 'Relative strand'] == "+":
+                    # check if the next one is a valid 18S
+                    if i + 2 < len(group) and '_18S' in group.at[i + 2, 'Target sequence name'] and group.at[i + 2, 'Relative strand'] == "+":
+                        morph = group.at[i, 'Query sequence name'], group.at[i, 'Query start'] - 100, group.at[i + 2, 'Query start'] - 100, '+'
+                        morphs.add(morph)
+            # start with a 18S in the minus strand
+            elif '_18S' in group.at[i, 'Target sequence name'] and group.at[i, 'Relative strand'] == "-":
+                #check if the next one is a valid 28S
+                if i + 1 < len(group) and '_28S' in group.at[i + 1, 'Target sequence name'] and group.at[i, 'Relative strand'] == "-":
+                    # check if the next one is a valid 18S
+                    if i + 2 < len(group) and '_18S' in group.at[i + 2, 'Target sequence name'] and group.at[i + 2, 'Relative strand'] == "-" and group.at[i + 2, 'Query end'] + 100 <= group.at[i, 'Query sequence length']:
+                        morph = group.at[i, 'Query sequence name'], group.at[i, 'Query end'] + 100, group.at[i + 2, 'Query end'] + 100, '-'
+                        morphs.add(morph)                    
 
             i += 1
     # Convert morphs set to DataFrame
@@ -74,7 +68,7 @@ def process_paf_alignments(data_df):
     return morphs_df
     # return morphs
 
-def extract_sequences(fasta_file, morphs_df, output_dir):
+def extract_sequences(fasta_file, morphs_df, output_dir, sampleid):
     sequences = SeqIO.to_dict(SeqIO.parse(fasta_file, "fasta"))
     extracted_sequences = []
 
@@ -84,21 +78,27 @@ def extract_sequences(fasta_file, morphs_df, output_dir):
         end = int(row['End'])
         strand = row['Strand']
 
+
         if seq_id in sequences:
             sequence = sequences[seq_id].seq[start:end]
             if strand == '-':
                 sequence = sequence.reverse_complement()
-            extracted_sequences.append((seq_id + ':' + str(start) + '-' + str(end), str(sequence)))
+            extracted_sequences.append((seq_id + ':' + str(start) + '-' + str(end) + ':' + strand, str(sequence)))
 
-    with open(os.path.join(output_dir, "rDNA.morphs.fasta"), 'w') as output_handle:
+    with open(os.path.join(output_dir, sampleid + "rDNA.morphs.fasta"), 'w') as output_handle:
         for seq_id, sequence in extracted_sequences:
             output_handle.write(f">{seq_id}\n{sequence}\n")
+
+    output_file = os.path.join(output_dir, sampleid + "rDNA.morphs.tsv")
+    morphs_df.to_csv(output_file, sep='\t', index=False, header=False)
 
 def main():
     parser = argparse.ArgumentParser(description="Process PAF alignments and extract sequences.")
     parser.add_argument('-p', '--paf', required=True, help='PAF alignments file')
     parser.add_argument('-f', '--fasta', required=True, help='FASTA file of query sequences')
     parser.add_argument('-o', '--output_dir', required=True, help='Output directory for results')
+    parser.add_argument('-s', '--sampleid', required=True, help='Sample ID for output files')
+
     args = parser.parse_args()
 
     if not os.path.isfile(args.paf):
@@ -110,7 +110,7 @@ def main():
 
     data_df = read_paf_file(args.paf)
     morphs_df = process_paf_alignments(data_df)
-    extract_sequences(args.fasta, morphs_df, args.output_dir)
+    extract_sequences(args.fasta, morphs_df, args.output_dir, args.sampleid)
 
 if __name__ == "__main__":
     main()
